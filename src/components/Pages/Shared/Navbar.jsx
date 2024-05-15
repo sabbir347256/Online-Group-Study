1337fb104fb1a6de894a6b6b7495b80e87423151
sabbir347256 <srgamingzone71@gmail.com>
2024-05-15 16:19:56 +0600

    full project is complete

--- src/components/Pages/Shared/Navbar.jsx
+++ src/components/Pages/Shared/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
                         </div>
                         <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                             <NavLink to='/myassignment'><li><a>My Attempted Assignments</a></li></NavLink>
-                            <li><button onClick={handleSignOut}>Logout</button></li>
+                            <li><button onClick={handleSignOut}>LogOut</button></li>
                         </ul>
                     </div>
                 }
