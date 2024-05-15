e83d72c84f5ec85f93f2e307daeca70591b3d164
sabbir347256 <srgamingzone71@gmail.com>
2024-05-15 16:16:29 +0600

    pending is update

--- src/components/Pages/Shared/Navbar.jsx
+++ src/components/Pages/Shared/Navbar.jsx
@@ -74,7 +74,7 @@ const Navbar = () => {
                     user && <div className="dropdown dropdown-end mr-3">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img alt="" src={user ? user?.photoURL : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} />
+                                <img alt="" src={user ? user?.photoURL : ''} />
                             </div>
                         </div>
                         <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
